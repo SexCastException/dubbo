a054b3a729d216414c91d92f0995913cc4c50b2a
pyh <pyh@qq.com>
2019-09-03 00:20:26 +0800

    .

--- dubbo-learning-example/user-service-provider/src/main/java/com/huazai/mall/MainApplication.java
+++ dubbo-learning-example/user-service-provider/src/main/java/com/huazai/mall/MainApplication.java
@@ -2,12 +2,10 @@ package com.huazai.mall;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.IOException;
-
 public class MainApplication {
-	public static void main(String[] args) throws IOException {
-		ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("provider.xml");
-		ioc.start();
-		System.in.read();
-	}
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("provider.xml");
+        ioc.start();
+        System.in.read();
+    }
 }
